2475f3a4fcddfd78be199fb5f89216972ab9c427
Filip Ceglik <fceglik@gmail.com>
2017-07-09 12:57:32 +0200

    chatbot page added

--- slackbot/chatbot/views.py
+++ slackbot/chatbot/views.py
@@ -4,3 +4,7 @@ from django.http import HttpResponse
 def index(request):
     my_dictionary = {'insert_me': "Hello I am from views.py"}
     return render(request, 'index.html', context=my_dictionary)
+
+def chatbotview(SiteView):
+    my_dict = {'example': "Chat with bot!"}
+    return render(SiteView, 'app.html', context=my_dict)
--- slackbot/slackbot/urls.py
+++ slackbot/slackbot/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.conf.urls import include
 from chatbot import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^chatbot/', include('chatbot.urls')),
     url(r'^admin/', admin.site.urls),
 ]
